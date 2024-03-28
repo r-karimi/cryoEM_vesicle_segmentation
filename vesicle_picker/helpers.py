85e36c3cc4be444a6e0a9caaba2ac9c207bc1f5a
Ryan Karimi <rk@nagai.sickkids.ca>
2024-03-28 15:22:52 -0400

    Initial commit

--- vesicle_picker/helpers.py
+++ vesicle_picker/helpers.py
@@ -14,6 +14,8 @@ def blockshaped(arr, nrows, ncols):
 
     If arr is a 2D array, the returned array should look like n subblocks with
     each subblock preserving the "physical" layout of arr.
+    
+    Taken from: https://stackoverflow.com/questions/16856788/slice-2d-array-into-smaller-2d-arrays
     """
     h, w = arr.shape
     assert h % nrows == 0, f"{h} rows is not evenly divisible by {nrows}"
@@ -29,6 +31,8 @@ def unblockshaped(arr, h, w):
 
     If arr is of shape (n, nrows, ncols), n sublocks of shape (nrows, ncols),
     then the returned array preserves the "physical" layout of the sublocks.
+    
+    Taken from: https://stackoverflow.com/questions/16856788/slice-2d-array-into-smaller-2d-arrays
     """
     n, nrows, ncols = arr.shape
     return (arr.reshape(h//nrows, -1, nrows, ncols)
@@ -36,7 +40,7 @@ def unblockshaped(arr, h, w):
                .reshape(h, w))
 
 def show_anns(anns):
-    """Adapted from segment-anything"""
+    """Adapted from the segment-anything Github repository"""
     if len(anns) == 0:
         return
     sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
@@ -62,13 +66,16 @@ def read_config(config_file_path):
     config.read(config_file_path)
     return config
 
-# Taken from: https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python/
 def factors(n):    
+    """
+    Taken from: https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python/
+    """
     return np.array(reduce(list.__add__, 
                 ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 
 def import_mrc(filename):
     """Use funcs_mrcio to open a specified .mrc file"""
+    
     # Read the .mrc file in binary
     micrograph = open(filename,'rb')
     
@@ -105,54 +112,4 @@ def export_mrc(image, filename):
     # Write the rebinned array to the new file
     funcs_mrcio.iwrsec_opened(image, mrc)
 
-def make_square_bbox(bbox):
-    # Unpack the bbox coordinates
-    x_min, y_min, x_max, y_max = bbox
-    
-    # Calculate the center point of the bbox
-    center_x = (x_min + x_max) / 2
-    center_y = (y_min + y_max) / 2
-    
-    # Calculate the width and height of the bbox
-    width = x_max - x_min
-    height = y_max - y_min
-    
-    # Determine the size of the square bbox
-    side_length = max(width, height)
-    
-    # Calculate new bbox coordinates to make it square
-    new_x_min = center_x - side_length / 2
-    new_y_min = center_y - side_length / 2
-    new_x_max = center_x + side_length / 2
-    new_y_max = center_y + side_length / 2
-    
-    # Return the new square bbox
-    return int(new_x_min), int(new_y_min), int(new_x_max), int(new_y_max)
 
-# Adapted from Jensen, Sigworth et al. 2015
-def circular_tukey_window(dims, Rn, alpha):
-    """
-    Generate a circular Tukey window with updated conditionals.
-
-    :param dims: Tuple of (height, width) for the window dimensions.
-    :param Rn: Radius up to which the window value is 1.
-    :param alpha: Controls the width of the tapering region.
-    """
-    # Generate a grid of indices
-    rows, cols = np.ogrid[:dims[0], :dims[1]]
-    
-    # Calculate distances from the center
-    c_row, c_col = np.array(dims) / 2  # Center of the image
-    r = np.sqrt((rows - c_row)**2 + (cols - c_col)**2)  # Actual radial distances
-    
-    # Initialize the window based on the conditionals
-    window = np.zeros(dims)
-    
-    # First condition: r <= Rn
-    window[r <= Rn] = 1
-    
-    # Second condition: Rn < r < Rn + 1/alpha
-    taper_mask = (r > Rn) & (r < Rn + 1/alpha)
-    window[taper_mask] = 0.5 + 0.5 * np.cos(np.pi * alpha * (r[taper_mask] - Rn))
-    
-    return window
